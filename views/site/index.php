a41c9f07282aa26efad5655b33dbe05f603e8fbb
Mateo M <fmmedina3@espe.edu.ec>
2023-12-11 20:55:08 +0100

    Datos sexo bdd en index

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
+        <h1 class="display-4">BIENVENIDOS!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
@@ -18,14 +18,12 @@ $this->title = 'My Yii Application';
 
         <div class="row">
             <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <h2>Lista de sexos</h2>
+                <?php 
+                    foreach($listaSexo as $item){
+                        echo "<p>".$item->SEX_NOMBRE."</p>";
+                    }
+                ?>
             </div>
             <div class="col-lg-4 mb-3">
                 <h2>Heading</h2>
