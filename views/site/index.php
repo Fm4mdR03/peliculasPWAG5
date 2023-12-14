8190d7ec25a2c925869a47b21ca7084cfe0f478e
Sinchiton <absinchiguano@espe.edu.ec>
2023-12-14 18:27:54 -0500

    Mas vistos 1.2

--- views/site/index.php
+++ views/site/index.php
@@ -26,31 +26,36 @@ use app\models\Pelicula;
     </div>
 
     <div class="body-content">
-    <div class="row">
+        <div class="row">
 
-        <?php
-        $peliculasAlquiladas = Pelicula::find()
-            ->select(['pelicula.PEL_NOMBRE', 'director.DIR_NOMBRE', 'genero.GEN_NOMBRE', 'pelicula.PEL_FECHA_ESTRENO'])
-            ->joinWith(['dIR', 'gEN', 'alquilers']) // Usa los nombres exactos de los métodos en tu modelo
-            ->groupBy(['pelicula.PEL_ID'])
-            ->orderBy(['COUNT(alquiler.ALQ_ID)' => SORT_DESC])
-            ->limit(4)
-            ->all();
+            <?php
+            $peliculasAlquiladas = Pelicula::find()
+                ->select(['pelicula.PEL_NOMBRE', 'pelicula.PEL_FECHA_ESTRENO'])
+                ->addSelect(['director.DIR_NOMBRE', 'genero.GEN_NOMBRE'])
+                ->leftJoin('director', 'pelicula.DIR_ID = director.DIR_ID')
+                ->leftJoin('genero', 'pelicula.GEN_ID = genero.GEN_ID')
+                ->joinWith('alquilers')
+                ->groupBy(['pelicula.PEL_ID'])
+                ->orderBy(['COUNT(alquiler.ALQ_ID)' => SORT_DESC])
+                ->limit(4)
+                ->asArray()  // Importante para que las relaciones se carguen correctamente en el resultado
+                ->all();
+            ?>
 
-        ?>
+            <?php foreach ($peliculasAlquiladas as $pelicula) : ?>
+                <div class="col-lg-3 mb-3">
+                    <h2><?= $pelicula['PEL_NOMBRE'] ?></h2>
+                    <p><strong>Director:</strong> <?= $pelicula['DIR_NOMBRE'] ?? 'N/A' ?></p>
+                    <p><strong>Género:</strong> <?= $pelicula['GEN_NOMBRE'] ?? 'N/A' ?></p>
+                    <p><strong>Fecha de Estreno:</strong> <?= $pelicula['PEL_FECHA_ESTRENO'] ?></p>
+                </div>
+            <?php endforeach; ?>
 
-        <?php foreach ($peliculasAlquiladas as $pelicula) : ?>
-            <div class="col-lg-3 mb-3">
-                <h2><?= $pelicula->PEL_NOMBRE ?></h2>
-                <p><strong>Director:</strong> <?= $pelicula->dIR ? $pelicula->dIR->DIR_NOMBRE : 'N/A' ?></p>
-                <p><strong>Género:</strong> <?= $pelicula->gEN ? $pelicula->gEN->GEN_NOMBRE : 'N/A' ?></p>
-                <p><strong>Fecha de Estreno:</strong> <?= $pelicula->PEL_FECHA_ESTRENO ?></p>
-            </div>
-        <?php endforeach; ?>
-
-
+        </div>
     </div>
-</div>
+
+
+
 
 
 </div>
